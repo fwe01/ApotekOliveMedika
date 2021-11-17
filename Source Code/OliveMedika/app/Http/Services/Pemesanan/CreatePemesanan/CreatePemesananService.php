e3eeb949cdd5373055fbf0e486d4c4d27af5490c
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-11-17 19:06:12 +0700

    Fix : Not using active promo on new pemesanan

--- Source Code/OliveMedika/app/Http/Services/Pemesanan/CreatePemesanan/CreatePemesananService.php
+++ Source Code/OliveMedika/app/Http/Services/Pemesanan/CreatePemesanan/CreatePemesananService.php
@@ -5,6 +5,7 @@ namespace App\Http\Services\Pemesanan\CreatePemesanan;
 use App\Exceptions\OliveMedikaException;
 use App\Http\Repositories\BarangRepository;
 use App\Http\Repositories\PemesananRepository;
+use App\Http\Repositories\PromoRepository;
 use App\Models\BarangPemesanan as BarangPemesananModels;
 use App\Models\Pemesanan;
 use App\Models\User;
@@ -13,15 +14,21 @@ class CreatePemesananService
 {
 	private BarangRepository $barang_repository;
 	private PemesananRepository $pemesanan_repository;
+	private PromoRepository $promo_repository;
 
 	/**
 	 * @param BarangRepository $barang_repository
 	 * @param PemesananRepository $pemesanan_repository
+	 * @param PromoRepository $promo_repository
 	 */
-	public function __construct(BarangRepository $barang_repository, PemesananRepository $pemesanan_repository)
-	{
+	public function __construct(
+		BarangRepository $barang_repository,
+		PemesananRepository $pemesanan_repository,
+		PromoRepository $promo_repository
+	) {
 		$this->barang_repository = $barang_repository;
 		$this->pemesanan_repository = $pemesanan_repository;
+		$this->promo_repository = $promo_repository;
 	}
 
 	/**
@@ -55,15 +62,19 @@ class CreatePemesananService
 		$barang_pemesanan = [];
 		foreach ($barangs as $barang) {
 			$barang_from_repo = $this->barang_repository->getBarangById($barang->getIdBarang());
+			$promo_barang = $this->promo_repository->getActivePromoByIdBarang($barang->getIdBarang());
 
 			if (!$barang_from_repo) {
 				throw OliveMedikaException::build('barang-not-found', 2016);
 			}
 
+			$harga = $barang_from_repo->getHarga() < $promo_barang[0]->getHargaPromoPerUnit() ?
+				$barang_from_repo->getHarga() : $promo_barang[0]->getHargaPromoPerUnit();
+
 			$barang_pemesanan[] = BarangPemesananModels::create(
 				$barang_from_repo->getId(),
 				$barang_from_repo->getNama(),
-				$barang_from_repo->getHarga(),
+				$harga,
 				$barang->getQuantity()
 			);
 		}
